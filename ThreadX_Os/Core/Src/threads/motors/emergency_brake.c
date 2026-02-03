0e3f205ee454d44c602b884fbdc726d6360a5d96
jose5556 <josefilmeneses@gmail.com>
2026-02-03 12:43:32 +0000

    Add emergency brake thread implementation for motor control

--- ThreadX_Os/Core/Src/threads/motors/emergency_brake.c
+++ ThreadX_Os/Core/Src/threads/motors/emergency_brake.c
@@ -4,7 +4,7 @@
 VOID  thread_emergency_brake(ULONG thread_input) 
 {
     t_rx_can_msg    msg;
-    t_rx_can_msg    last_msg;
+    //t_rx_can_msg    last_msg;
 
     tx_thread_sleep(100); // Allow other threads to initialize
     while (1) 
@@ -13,7 +13,7 @@ VOID  thread_emergency_brake(ULONG thread_input)
         {
             motor_set(MOTOR_LEFT, 0, 1);
             motor_set(MOTOR_RIGHT, 0, 1);
-            tx_thread_sleep(1000);
+            tx_thread_sleep(500);
         }
     }
 }
